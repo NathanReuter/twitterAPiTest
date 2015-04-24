a3a4e0a3beadc4b717b48a30b71255116ea1a13f
Nathan Reuter <nathan0reuter@gmail.com>
2015-04-24 19:31:36 -0300

    Adding first files.

---
+++ twitter.js
@@ -0,0 +1,37 @@
+var Twitter = require('twitter'),
+    _ = require('lodash'),
+    moment = require('moment'),
+    profileName = {
+        screen_name: 'nathan0reuter'
+    },
+    client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
+
+var getTimeLine = function (profileName, callback) {
+    client.get('statuses/user_timeline', profileName, function (err, tweets, res) {
+        if (!err) {
+            var timeLineMessages = '';
+            _.each(tweets, function (twit, index) {
+                if ((index + 1) !== tweets.length) {
+                    console.log(index, tweets.lengt);
+                    timeLineMessages += showFormatText(twit.created_at, twit.text, true);
+                } else {
+                    timeLineMessages += showFormatText(twit.created_at, twit.text);
+                }
+            });
+            callback(timeLineMessages);
+        }
+    });
+},
+    showFormatText = function (date, text, newLine) {
+        return 'Creation Date: ' + moment(date).format('DD-MM-YYYY, h:mm:ss') 
+            + ' # Message: ' + text + (newLine ? '\n' : '');
+    };
+
+getTimeLine(profileName, function(twits) {
+    console.log(twits);
+});
